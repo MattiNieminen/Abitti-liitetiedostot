848b36ae6ce0cf4a22913efca1e2a77805705b85
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2017-11-19 01:25:42 +0200

    Implement zip functionality

--- src/abitti_liitetiedostot/core.cljs
+++ src/abitti_liitetiedostot/core.cljs
@@ -1,5 +1,6 @@
 (ns abitti-liitetiedostot.core
-    (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [abitti-liitetiedostot.zip :as zip]))
 
 (defonce state (r/atom {}))
 
@@ -13,7 +14,7 @@
      [:h2.attachments__header "Valitut liitteet"]
      [:ul.attachments-list
       (for [[idx itm] (map-indexed vector attachments)]
-        [:li.attachments-list__item {:key idx} (:filename itm)])]]))
+        [:li.attachments-list__item {:key idx} (.-name itm)])]]))
 
 (defn content-view []
   (let [{:keys [exam attachments]} @state]
@@ -26,8 +27,7 @@
       {:type "file"
        :on-change (fn [e]
                     (let [f (aget (-> e .-target .-files) 0)]
-                      (swap! state assoc :exam {:file f
-                                                :filename (.-name f)})))}]
+                      (swap! state assoc :exam f)))}]
      [:p.content__text
       "Klikkaa alla olevaa nappia, ja valitse kaikki liitetiedostot jotka haluat liittää kokeeseen. Voit valita useamman tiedoston pitämällä näppämistön CMD-nappia tai CTRL-nappia pohjassa"]
      [:input.content__file
@@ -38,21 +38,20 @@
                       (swap! state
                              assoc
                              :attachments
-                             (for [idx (range (.-length files))
-                                   :let [f (aget files idx)]]
-                               {:file f
-                                :filename (.-name f)}))))}]
+                             (for [idx (range (.-length files))]
+                               (aget files idx)))))}]
      [attachments-view {:attachments attachments}]
      [:p.content__text
       "Klikkaa alla olevaa nappia ladataksesi uuden zip-tiedoston koneellesi, jonka voit tuoda takaisin Abittiin klikkaamalla \"Tuo koe (.zip)\" -nappia. Valitset vain tuolloin Abitista tiedoston jonka saat klikkaamalla alla olevaa nappia."]
-     (if (empty? exam)
+     (if-not exam
        [:p.content__text.content__text--warning
         "Et ole vielä valinnut Abitista tuotua zip-tiedostoa"])
      (if (empty? attachments)
        [:p.content__text.content__text--warning
         "Et ole vielä valinnut liitteitä"])
      [:button.content__button
-      {:disabled (and (empty? exam) (empty? attachments))}
+      {:disabled (or (nil? exam) (empty? attachments))
+       :on-click #(zip/abitti-with-attachments exam attachments!))}
       "Lataa valmis koe zip-tiedostona Abittiin viemistä varten"]]))
 
 (defn main-view []
---
+++ src/abitti_liitetiedostot/zip.cljs
@@ -0,0 +1,33 @@
+(ns abitti-liitetiedostot.zip
+  (:require [cljsjs.jszip]))
+
+(defn create-zip []
+  (js/JSZip.))
+
+(defn import-zip [file]
+  (.loadAsync (create-zip) file))
+
+(defn add-file [z filename file]
+  (.file z filename file))
+
+(defn ->base64 [z]
+  (.generateAsync z (clj->js {:type "base64"})))
+
+;; TODO clean promises
+(defn abitti-with-attachments [exam attachments]
+  (let [zipped-attachments (create-zip)]
+    (doseq [a attachments]
+      (add-file zipped-attachments (.-name a) a))
+    (.then (import-zip exam)
+           (fn [z]
+             (.then (->base64 zipped-attachments)
+                    (fn [base64]
+                      (add-file z "attachments.zip" base64)
+                      (.then (->base64 z)
+                             (fn [base64]
+                               (aset js/window
+                                     "location"
+                                     (str "data:application/zip;base64,"
+                                          base64)))))))))))
+
+
